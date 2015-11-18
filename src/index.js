1cbe2159d5d79d342b774a10408c1e38732ed59c
azu <azuciao@gmail.com>
2015-11-19 00:26:32 +0900

    feat(rule): add ranking-meishi

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 export default {
     rules: {
         "share-of-char-type": require("./rules/share-of-char-type"),
-        "tateishi-level": require("./rules/tateishi-level")
+        "tateishi-level": require("./rules/tateishi-level"),
+        "ranking-meishi": require("./rules/ranking-meishi")
     }
 }
